1a154a86858accf82f9828af0027fc415acfdbc3
Muhammad Hilal Fanani <mhfanany@gmail.com>
2026-06-08 14:54:31 +0700

    port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const db = require('./db.js');
-const port = 5775;
+const PORT = process.env.PORT || 3000;
 
 const http = require("http");
 const { Server } = require("socket.io");
